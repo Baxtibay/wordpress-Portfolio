dd0f0b8975237cb30daad1d9ce7257dd3c0ef737
Baxtibay <baxtibaybekniyazov@gmail.com>
2025-11-28 00:09:32 +0500

    Change comments in lang-loggler.js

--- js/lang-toggler.js
+++ js/lang-toggler.js
@@ -12,21 +12,21 @@ document.querySelectorAll(".language-switcher__item").forEach(item => {
         const flagClass = item.querySelector(".language-switcher__flag").classList.value;
         const text = item.querySelector(".language-switcher__label").innerText;
 
-        // Flagni almashtirish
+        // Change flag icons
         const selectedFlag = selected.querySelector(".language-switcher__flag");
         selectedFlag.className = flagClass;
 
-        // Labelni almashtirish
+        // Change label
         selected.querySelector(".language-switcher__label").innerText = text;
 
-        // lang atributini yangilash
+        // Update lang attribute
         selected.setAttribute("data-lang", lang);
 
         dropdown.style.display = "none";
     });
 });
 
-// Dropdowndan tashqariga bosilganda yopish
+// Close when clicked outside of dropdown
 document.addEventListener("click", (e) => {
     if (!e.target.closest(".language-switcher")) {
         dropdown.style.display = "none";
