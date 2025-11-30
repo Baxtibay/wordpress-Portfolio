44c4d32ab25d1638354ab37c8842051ed8e0c483
Baxtibay <baxtibaybekniyazov@gmail.com>
2025-11-30 14:46:01 +0500

    Change the functionality of the site-header toggler-button

--- js/menu-toggler.js
+++ js/menu-toggler.js
@@ -1,8 +1,31 @@
 const modifiers = {
     elSiteHeaderOpen: 'site-header--open',
-}
+    bodyLocked: 'body--locked'
+};
 
 const elSiteHeader = document.querySelector('.site-header');
-const elSiteHeaderToggler = elSiteHeader.querySelector('.site-header__toggler');
+const elSiteHeaderToggler = document.querySelector('.site-header__toggler');
+const elTogglerIcon = document.querySelector('.site-header__toggler-icon');
+const body = document.body;
 
-if (elSiteHeaderToggler) {elSiteHeaderToggler.addEventListener('click', () => elSiteHeader.classList.toggle(modifiers.elSiteHeaderOpen))}
+// CLICK Toggler
+elSiteHeaderToggler.addEventListener('click', () => {
+    const isOpen = elSiteHeader.classList.toggle(modifiers.elSiteHeaderOpen);
+
+    body.classList.toggle(modifiers.bodyLocked, isOpen);
+    elTogglerIcon.textContent = isOpen ? 'close' : 'menu';
+});
+
+// Close when clicked outside the navbar
+document.addEventListener('click', (evt) => {
+
+    const clickOutsideNavbar = 
+        !elSiteHeader.contains(evt.target) &&
+        !elSiteHeaderToggler.contains(evt.target);
+
+    if (clickOutsideNavbar && elSiteHeader.classList.contains(modifiers.elSiteHeaderOpen)) {
+        elSiteHeader.classList.remove(modifiers.elSiteHeaderOpen);
+        body.classList.remove(modifiers.bodyLocked);
+        elTogglerIcon.textContent = 'menu';
+    }
+});
